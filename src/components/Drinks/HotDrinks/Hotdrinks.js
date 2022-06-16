b14500d87672864ff091baf43eb892286eadd745
RomanPalna <roma.palna@gmail.com>
2022-06-16 14:39:30 +0300

    add new files

---
+++ src/components/Drinks/HotDrinks/Hotdrinks.js
@@ -0,0 +1,11 @@
+export default function HotDrinks() {
+  return (
+    <ul>
+      <li>Espresso</li>
+      <li>Americano</li>
+      <li>Doppio</li>
+      <li>Cappucino</li>
+      <li>Latte</li>
+    </ul>
+  );
+}
---
+++ src/components/Non-alcohol/Non-alcohol.js
@@ -0,0 +1,5 @@
+import Container from "../Container/Continer";
+
+export default function NonAlcohol() {
+  return <Container></Container>;
+}
