7306556508879854da03201943474cdf40e10860
RomanPalna <roma.palna@gmail.com>
2022-06-18 17:07:16 +0300

    list to tablerow'

--- src/components/Drinks/HotDrinks/HotDrinksList.js
+++ src/components/Drinks/HotDrinks/HotDrinksList.js
@@ -1,10 +1,10 @@
 export default function HotDrinksList({ props }) {
   const { name, litres, cost } = props;
   return (
-    <li>
-      <span>{name}</span>
-      <span>{litres}</span>
-      <span>{cost}</span>
-    </li>
+    <tr>
+      <td className="">{name}</td>
+      <td>{litres} мл.</td>
+      <td>{cost} грн.</td>
+    </tr>
   );
 }
--- src/components/Drinks/HotDrinks/Hotdrinks.js
+++ src/components/Drinks/HotDrinks/Hotdrinks.js
@@ -5,11 +5,21 @@ import HotDrinksList from "./HotDrinksList";
 export default function HotDrinks() {
   return (
     <Modal>
-      <ul className="forrest__coffee">
-        {coffee.coffee.map((drink) => {
-          return <HotDrinksList key={drink.id} props={drink} />;
-        })}
-      </ul>
+      <table className="forrest__coffee">
+        <caption className="forrest__coffee--head">Кава</caption>
+        <thead>
+          <tr>
+            <th scope="col">Назва</th>
+            <th scope="col">Об'єм</th>
+            <th scope="col">Ціна</th>
+          </tr>
+        </thead>
+        <tbody>
+          {coffee.coffee.map((drink) => {
+            return <HotDrinksList key={drink.id} props={drink} />;
+          })}
+        </tbody>
+      </table>
     </Modal>
   );
 }
