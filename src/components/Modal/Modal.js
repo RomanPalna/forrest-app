7a67e885419ce01410003f7209a55af3db7282b3
RomanPalna <roma.palna@gmail.com>
2022-06-23 21:34:31 +0300

    add some fixes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,9 +1,18 @@
+import { GoChevronLeft } from "react-icons/go";
+
 export default function Modal({ active, setActive, children }) {
   return (
     <div
       className={active ? "Modal__backdrop active" : "Modal__backdrop"}
       onClick={() => setActive(false)}
     >
+      <button
+        className="modal__button"
+        type="button"
+        onClick={() => setActive(false)}
+      >
+        <GoChevronLeft fill={"#E4E4E4"} />
+      </button>
       <div
         className={active ? "Modal__content active" : "Modal__content"}
         onClick={(event) => event.stopPropagation()}
